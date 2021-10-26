81b60d87119cd28a909653329fcc708d5bf3a567
Conner Gleason <connereg@optonline.net>
2021-10-26 17:41:36 -0400

    Backend: Basic table Structure

--- db/migrate/20211026213124_create_transferrable_transactions.rb
+++ db/migrate/20211026213124_create_transferrable_transactions.rb
@@ -1,12 +1,13 @@
 class CreateTransferrableTransactions < ActiveRecord::Migration[6.1]
   def change
     create_table :transferrable_transactions do |t|
-      t.integer :transactor_id 
+      t.integer :transactor_id
       t.integer :transactee_id
       t.float :cost
       t.string :category
       t.string :message
-      
+      t.boolean :confirmed
+      t.boolean :transaction_to_user
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!( username: "Conner", balance: 88.50, remainder: 20.20)
