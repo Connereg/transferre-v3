f7cc640013cae2eaea930893ef69e42476141535
Isabella Mighell <ikm4dm@virginia.edu>
2021-10-27 09:44:15 -0400

    secure login authorization and authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   resources :user_expenses
   resources :transferrable_transactions
-  resources :users
+  resources :users, only: [:update, :destroy]
 
   #check out for frontend fetches https://learning.flatironschool.com/courses/4231/pages/authenticating-users?module_item_id=311665
 
   #for login currently just username no password
   post "/login", to: "sessions#create"
+  #create new user
+  post "/signup", to: "users#create"
   #allows user to stay logged in 
-  get "/auth", to: "users#show"
+  get "/me", to: "users#show"
   #to log the user out 
   delete "/logout", to: "sessions#destroy"
 
