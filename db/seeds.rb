7e7bbc1b7d39871b9e0ee6bd9b0039f0975b1906
Conner Gleason <connereg@optonline.net>
2021-10-27 23:35:41 -0400

    Back End Updates: Conner

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "🌱 Seeding Users..."
+5.times do
+    User.create(username: Faker::Name.name, password: "123")
+end
