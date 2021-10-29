a04aefcf6e178b4db09d6368f63bb8b19aa18203
Isabella Mighell <ikm4dm@virginia.edu>
2021-10-29 07:43:24 -0400

    fixed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
 
    has_many :user_expenses, dependent: :destroy
    
-   has_many :transactor_transactions, foreign_key: :transactee_id, class_name: "Transferrable_Transaction", dependent: :destroy
+   has_many :transactor_transactions, foreign_key: :transactee_id, class_name: "TransferrableTransaction", dependent: :destroy
    has_many :transactors, through: :transactor_transactions, source: :transactor
 
   
-   has_many :transactee_transactions, foreign_key: :transactor_id, class_name: "Transferrable_Transaction", dependent: :destroy    
+   has_many :transactee_transactions, foreign_key: :transactor_id, class_name: "TransferrableTransaction", dependent: :destroy    
    has_many :transactees, through: :transactee_transactions, source: :transactee
 
    def initialize(attributes=nil)
